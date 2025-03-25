4625d29d9911ac00a54f592754da1ef5dd862a9c
Patrick Schlosser <pschloss@nd.edu>
2025-03-24 22:53:00 -0400

    update header and imports

--- frontend/sidequest/src/Components/Components.js
+++ frontend/sidequest/src/Components/Components.js
@@ -1,5 +1,6 @@
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import MainPage from './Main/MainPage.js';
+import Header from './Header/Header.js';
 import { Navigate } from 'react-router-dom';
 
 export default function Components() {
--- frontend/sidequest/src/Components/Main/MainPage.js
+++ frontend/sidequest/src/Components/Main/MainPage.js
@@ -4,6 +4,7 @@ import './styles.css';
 const MainPage = () => {
     return (
         <div className="main-page">
+            <h1>Welcome to SideQuest!</h1>
         </div>
     );
 };
