6a2a18af00823ef96baac91185e9ce3d5f3c5ffc
Patrick Schlosser <pschloss@nd.edu>
2025-03-24 22:41:21 -0400

    create front end

---
+++ frontend/sidequest/src/Components/Components.js
@@ -0,0 +1,17 @@
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import MainPage from './Main/MainPage.js';
+import { Navigate } from 'react-router-dom';
+
+export default function Components() {
+    return (
+        <Router style="background-color: #8CBA80;">
+            <Header/>
+            <br/>
+            <Routes>
+                <Route path="/" element={<MainPage/>} />
+                <Route path="*" element={<Navigate to="/auth" replace />} /> 
+            </Routes>
+            <br/>
+        </Router>
+    );
+}
---
+++ frontend/sidequest/src/Components/Main/MainPage.js
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+import './styles.css';
+
+const MainPage = () => {
+    return (
+        <div className="main-page">
+        </div>
+    );
+};
+  
+export default MainPage;
