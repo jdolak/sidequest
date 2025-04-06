c78c6848cdcbb3e9f8d082971f267bec805cbe4d
Calista Suwita <csuwita@nd.edu>
2025-04-06 14:36:36 -0400

    sidebar

--- frontend/sidequest/src/Components/Components.js
+++ frontend/sidequest/src/Components/Components.js
@@ -1,18 +1,20 @@
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Sidebar from './Sidebar/Sidebar.js';
 import MainPage from './Main/MainPage.js';
 import Header from './Header/Header.js';
 import { Navigate } from 'react-router-dom';
 
 export default function Components() {
     return (
-        <Router style="background-color: #8CBA80;">
-            <Header/>
-            <br/>
-            <Routes>
-                <Route path="/" element={<MainPage/>} />
-                <Route path="*" element={<Navigate to="/auth" replace />} /> 
-            </Routes>
-            <br/>
-        </Router>
+        <Sidebar />
+        // <Router style="background-color: #8CBA80;">
+        //     <Header/>
+        //     <br/>
+        //     <Routes>
+        //         <Route path="/" element={<MainPage/>} />
+        //         <Route path="*" element={<Navigate to="/auth" replace />} /> 
+        //     </Routes>
+        //     <br/>
+        // </Router>
     );
 }
---
+++ frontend/sidequest/src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import searchIcon from '../../assets/images/search.svg';
+import './styles.css';
+
+const Sidebar = () => {
+    return (
+        <div class="sidebar">
+            <div class="groups">
+                <img src={searchIcon} />
+            </div>
+            <div class="profile">
+                <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 64 64" fill="none">
+                    <circle cx="32" cy="32" r="30" fill="#F3F4FB" stroke="#EAECF9" stroke-width="4"/>
+                </svg>
+            </div>
+        </div>
+    );
+};
+
+export default Sidebar;
