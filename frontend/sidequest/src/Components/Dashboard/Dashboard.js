e1e662979c3e47c0ce3c3fc214173a4262aafbd3
Calista Suwita <csuwita@nd.edu>
2025-04-07 12:30:19 -0400

    header

--- frontend/sidequest/src/Components/Dashboard/Dashboard.js
+++ frontend/sidequest/src/Components/Dashboard/Dashboard.js
@@ -5,6 +5,13 @@ import "./dashboard.css";
 const Dashboard = () => {
     return (
         <div class="dashboard">
+            <div class="group-header">
+                <div class="heading">Flaherty Hall</div>
+                <div>20 members</div>
+                <div class="coins">
+                    <div class="coin-text">100 coins</div>
+                </div>
+            </div>
             <QuestSection />
             <BetSection />
         </div>
