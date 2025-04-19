ccc3f579d1af6152e35bde5ba0c5648145dbf527
Jachob Dolak <dolak.jachob@gmail.com>
2025-04-19 19:18:24 -0400

    CORS nginx

--- backend/run.py
+++ backend/run.py
@@ -1,8 +1,6 @@
 from sqapp import create_app
 from dotenv import load_dotenv
 from sqapp.src.auth import BCRYPT
-from flask_cors import CORS
-
 
 import os
 
@@ -19,10 +17,6 @@ BCRYPT.init_app(sq_app)
 allowed_origins = [
     "http://localhost:{HOST_PORT}",
 ]
-
-CORS(sq_app)
-
-
 
 if __name__ == "__main__":
     sq_app.run(debug=True, host="0.0.0.0", port=5000, use_reloader=False)
