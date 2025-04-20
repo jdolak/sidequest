af6a6d164ddb768feac66431ac5ff56acedb0526
Jachob Dolak <dolak.jachob@gmail.com>
2025-04-19 23:50:05 -0400

    s3

--- backend/sqapp/src/uploads.py
+++ backend/sqapp/src/uploads.py
@@ -8,15 +8,15 @@ load_dotenv()
 S3_CLIENT = boto3.client(
     's3',
     endpoint_url='http://sidequest-sq-s3-1::9000', 
-    aws_access_key_id=os.getenv('MINIO_USER', 'minioadmin'),
-    aws_secret_access_key=os.getenv('MINIO_PASSWORD', 'minioadmin'),
+    aws_access_key_id=os.getenv('S3_ACCESS_KEY'),
+    aws_secret_access_key=os.getenv('S3_SECRET_KEY',),
 )
 
 def get_upload_url(filename):
     
     url = S3_CLIENT.generate_presigned_url(
         'get_object',
-        Params={'Bucket': 'my-bucket', 'Key': filename},
+        Params={'Bucket': 'quest-submissions', 'Key': filename},
         ExpiresIn=3600  # 1 hour
     )
 
