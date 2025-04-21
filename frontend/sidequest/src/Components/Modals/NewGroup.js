b2bde139356d4bffce1014fe5b2c3568ae2ecb39
Calista Suwita <csuwita@nd.edu>
2025-04-21 19:28:47 -0400

    create new group button works

--- frontend/sidequest/src/Components/Modals/NewGroup.js
+++ frontend/sidequest/src/Components/Modals/NewGroup.js
@@ -22,26 +22,29 @@ const NewGroupModal = ({ onClose }) => {
     }
 
     return (
-        <div className="modal-container">
-            <div className="modal-head">
-                <div className="modal-heading">Create New Group</div>
-                <img src={Close} onClick={onClose}/>
-            </div>
-
-            <form onSubmit={handleSubmit} className="group-form">
-                <div className="form-data">
-                    <label className="form-label">
-                            Group name
-                            <input className="form-input" type="text" name="groupname" value={formData.groupname} onChange={handleChange} required />
-                    </label>
-                    <label className="form-label">
-                            Group description
-                            <textarea className="form-input" name="groupdesc" value={formData.groupdesc} onChange={handleChange} rows={4} />
-                    </label>
+        <div className="modal-overlay">
+            <div className="modal-container">
+                <div className="modal-head">
+                    <div className="modal-heading">Create New Group</div>
+                    <img src={Close} className="close-btn" onClick={onClose}/>
                 </div>
-                <button className="submit-button" type="submit">Create Group</button>
-            </form>
+
+                <form onSubmit={handleSubmit} className="group-form">
+                    <div className="form-data">
+                        <label className="form-label">
+                                Group name
+                                <input className="form-input" type="text" name="groupname" value={formData.groupname} onChange={handleChange} required />
+                        </label>
+                        <label className="form-label">
+                                Group description
+                                <textarea className="form-input" name="groupdesc" value={formData.groupdesc} onChange={handleChange} rows={4} />
+                        </label>
+                    </div>
+                    <button className="submit-button" type="submit">Create group</button>
+                </form>
+            </div>
         </div>
+       
     )
 }
 
