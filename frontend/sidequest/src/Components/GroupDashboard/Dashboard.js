998fcd292563b5b5387d4a83390d4ab2020fdee5
Patrick Schlosser <pschloss@nd.edu>
2025-04-22 19:24:57 -0400

    add gropu info to dash

--- frontend/sidequest/src/Components/GroupDashboard/Dashboard.js
+++ frontend/sidequest/src/Components/GroupDashboard/Dashboard.js
@@ -1,15 +1,25 @@
 import React, {useEffect,useState} from "react";
+import { useParams } from "react-router-dom";
 import QuestSection from "./QuestSection/QuestSection";
 import BetSection from "./BetSection/BetSection";
 import "./dashboard.css";
 import { getOpenQuests } from "../../Services/Quests";
 import { getAllBets } from "../../Services/Bets";
+import { getGroup } from "../../Services/Groups";
 
 const Dashboard = () => {
     const [openBets, setOpenBets] = useState([]);
     const [openQuests, setOpenQuests] = useState([]);
+    const [group, setGroup] = useState({});
+
+    const { id } = useParams();
 
     useEffect(() => {
+        getGroup(id).then((response) => {
+            setGroup(response);
+        }).catch((error) => {
+            console.error("Error fetching group:", error);
+        });
         getAllBets().then((response) => {
                 setOpenBets(response);
             }).catch((error) => {
@@ -25,10 +35,10 @@ const Dashboard = () => {
     return (
         <div class="dashboard">
             <div class="group-header">
-                <div class="heading">Flaherty Hall</div>
-                <div>20 members</div>
+                <div class="heading">{group.group_desc}</div>
+                <div>{group.members} members</div>
                 <div class="coins">
-                    <div class="coin-text">100 coins</div>
+                    <div class="coin-text">{group.coins} coins</div>
                 </div>
             </div>
             <QuestSection quests={openQuests.slice(0,4)}/>
