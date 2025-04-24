0296a9da1026f812c6d17d80b6d405a4345a0a7f
Patrick Schlosser <pschloss@nd.edu>
2025-04-24 13:01:34 -0400

    pull groups and loop through them for sidebar buttons

--- frontend/sidequest/src/Components/Sidebar/Sidebar.js
+++ frontend/sidequest/src/Components/Sidebar/Sidebar.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {useEffect,useState} from "react";
 import searchIcon from '../../assets/images/search.svg';
 import {Link} from "react-router-dom";
 import './sidebar.css';
 import { useGlobalStore } from '../../stores/globalStore.js';
+import { getAllGroups } from "../../Services/Groups.js";
+
 
 const Sidebar = () => {
-    //  commenting out so front end can work for a bit
-    // const setCurrGroup = useGlobalStore((state) => state.setGroup);
+    const [groups, setGroups] = useState([]);
+    const setCurrGroup = useGlobalStore((state) => state.setGroup);
+
+    useEffect(() => {
+        getAllGroups().then((response) => {
+            setGroups(response);
+        }).catch((error) => {
+            console.error("Error fetching groups:", error);
+        });
+    }, []);
+
+
     return (
         <div className="sidebar">
             <div className="menu">
@@ -14,12 +26,13 @@ const Sidebar = () => {
                     <img src={searchIcon} />
                 </Link>
                 <div className="groups">
-                    {/* <button className="group-button" key={1} onClick={() => setCurrGroup(key)}> */}
-                    <button className="group-button">
-                        <div className="group">
-                            G1
-                        </div>
-                    </button>
+                    {groups.map((group, index) => (
+                        <button className="group-button" key={index} onClick={() => setCurrGroup(group.group_id)}>
+                            <div className="group">
+                                {group.name.slice(0, 2).toUpperCase()}
+                            </div>
+                        </button>
+                    ))}
                 </div>
             </div>
         </div>
