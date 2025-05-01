c64d8a2c76db2f1a6d2c5bae531d00bc1875af2a
Patrick Schlosser <pschloss@nd.edu>
2025-05-01 11:46:17 -0400

    use session store for global-store

--- frontend/sidequest/src/stores/globalStore.js
+++ frontend/sidequest/src/stores/globalStore.js
@@ -1,7 +1,23 @@
 // stores/globalStore.js
-import { create } from 'zustand';
+// import { create } from 'zustand';
 
-export const useGlobalStore = create((set) => ({
-  currGroupID: null,
-  setGroup: (value) => set({ currGroupID: value }),
-}));
+// export const useGlobalStore = create((set) => ({
+//   currGroupID: null,
+//   setGroup: (value) => set({ currGroupID: value }),
+// }));
+
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+export const useGlobalStore = create(
+  persist(
+    (set) => ({
+      currGroupID: null,
+      setGroup: (value) => set({ currGroupID: value }),
+    }),
+    {
+      name: 'global-store',
+      storage: sessionStorage, // Use sessionStorage instead of localStorage
+    }
+  )
+);
