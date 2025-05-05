5d18fe8b6cc971a537aaccba00cb673b061a0978
Jachob Dolak <dolak.jachob@gmail.com>
2025-05-05 02:19:02 -0400

    money checks

---
+++ backend/sqapp/src/utils.py
@@ -0,0 +1,11 @@
+from flask import g
+from sqapp.db import sql_one
+
+def can_afford(group_id, amount):
+    """
+    Check if the user is broke in the given group.
+    """
+    result = sql_one(g.db_session, "SELECT currency FROM SQ_GROUPS WHERE group_id = :group_id AND user_id = :user_id", {"group_id": group_id, "user_id": g.user})
+    if result["currency"] - amount > 0:
+        return True
+    return False
