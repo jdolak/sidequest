b4ae6c6bb1eae7e285ffdc46e9e7a2fcbb5a465a
Jachob Dolak <dolak.jachob@gmail.com>
2025-05-05 02:24:52 -0400

    leave group

--- backend/sqapp/src/deletion.py
+++ backend/sqapp/src/deletion.py
@@ -1,6 +1,7 @@
 from sqlalchemy import text
 from sqapp import LOG
 from flask import g, jsonify
+from sqapp.src.auth import group_member_count
 
 def delete_bet(bet_id):
     try:
@@ -35,5 +36,20 @@ def delete_group(group_id):
         LOG.error(f"Error deleting group: {e}")
         g.db_session.rollback()
         return jsonify({"error": "Failed to delete group"}), 500
+    
+def leave_group(group_id):
+    try:
+        sql = "DELETE FROM SQ_GROUPS WHERE group_id = :group_id AND user_id = :user_id"
+        g.db_session.execute(text(sql), {"group_id": group_id, "user_id": g.user})
+
+        if not group_member_count(group_id):
+            delete_group(group_id)
+            
+        LOG.info(f"User {g.user} left group with ID {group_id} successfully.")
+        return jsonify({"message": "Left group successfully"}), 200
+    except Exception as e:
+        LOG.error(f"Error leaving group: {e}")
+        g.db_session.rollback()
+        return jsonify({"error": "Failed to leave group"}), 500
 
 
