609326f03ab47e5de62c5e59473238b1282d9326
Jachob Dolak <dolak.jachob@gmail.com>
2025-05-05 03:08:58 -0400

    fixed

--- backend/sqapp/src/utils.py
+++ backend/sqapp/src/utils.py
@@ -6,6 +6,6 @@ def can_afford(group_id, amount):
     Check if the user is broke in the given group.
     """
     result = sql_one(g.db_session, "SELECT currency FROM SQ_GROUPS_USER WHERE group_id = :group_id AND user_id = :user_id", {"group_id": group_id, "user_id": g.user})
-    if result["currency"] - amount > 0:
+    if result["currency"] - int(amount) > 0:
         return True
     return False
