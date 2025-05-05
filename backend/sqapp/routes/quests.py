6bfdf7681dfbe4f5c15dd4f2832fa62ce37f8adf
Jachob Dolak <dolak.jachob@gmail.com>
2025-05-04 20:33:10 -0400

    fixed dupliacate accepted quests

--- backend/sqapp/routes/quests.py
+++ backend/sqapp/routes/quests.py
@@ -38,7 +38,7 @@ def get_quest_accepted(group_id):
     return sql_response(
         sql_many(
             g.db_session,
-            "SELECT q.quest_id, group_id, author_id, quest_title, quest_desc, reward_amount, due_date, quest_status, username FROM QUESTS q, QUEST_SUBMISSIONS qs, SQ_USERS u WHERE (LOWER(qs.status) = 'accepted' OR LOWER(qs.status) = 'resolved') AND q.quest_id = qs.quest_id AND q.group_id = :group_id AND u.user_id = q.author_id",
+            "SELECT DISTINCT q.quest_id, group_id, author_id, quest_title, quest_desc, reward_amount, due_date, quest_status, username FROM QUESTS q, QUEST_SUBMISSIONS qs, SQ_USERS u WHERE (LOWER(q.status) = 'accepted' OR LOWER(q.status) = 'resolved') AND q.quest_id = qs.quest_id AND q.group_id = :group_id AND u.user_id = qs.user_id",
             {"group_id": group_id},
         )
     )
